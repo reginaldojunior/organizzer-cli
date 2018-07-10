34b763833b80a16d402b8c27950eb09626aa7c53
Matheus Hernandes <midia.matheus@gmail.com>
2018-07-09 22:59:54 -0300

    add some cool things

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,62 @@
 #! /usr/bin/env node
 
-const arguments    = process.argv.splice(2, process.argv.length -1).join(' ')
-const Transactions = require('../models/Transactions')
-const Help         = require('../models/Help')
-const prettyjson   = require('prettyjson')
+const Transactions = require('../models/Transactions');
+const InitUser = require('../models/Init');
+const argv = require('minimist')(process.argv.slice(2));
 
-if (arguments === 'help') 
-    Help.listCommands()
+const Configstore = require('configstore');
+const config = new Configstore('organizze');
 
-if (arguments === 'list_transactions') 
-    Transactions.getTransactionsCurrentMonth.then(response => {
-        console.log(prettyjson.render(response))
-    })
+const prettyjson = require('prettyjson');
+const chalk = require('chalk');
+const commands = {
+  list_transactions: {
+    description: 'List transactions of current month',
+    exec: Transactions.getTransactionsCurrentMonth
+  },
+  init: {
+    description: 'Do the auth thing',
+    exec: InitUser
+  }
+};
+let firstCommand = argv._.shift();
+
+if (!config.username || !config.token) {
+  firstCommand = 'init';
+}
+
+if (
+  !firstCommand ||
+  Object.keys(commands).indexOf(firstCommand) === -1 ||
+  firstCommand === 'help'
+) {
+  if (firstCommand !== 'help') {
+    console.log(chalk.blue('Sorry but this command does not exists :('));
+  }
+
+  let commandsData = {};
+
+  Object.keys(commands).forEach(k => {
+    let obj = commands[k];
+    delete obj.exec;
+    commandsData[k] = obj;
+  });
+
+  console.log(prettyjson.render(commandsData));
+  process.exit();
+}
+
+const run = async () => {
+  try {
+    // Need to think about some way of args injection
+    let results = await commands[firstCommand].exec();
+    console.log(prettyjson.render(results));
+    return 0;
+  } catch (e) {
+    console.error(chalk.red(e));
+    return 0;
+  }
+};
+
+run().then(() => process.exit());
+
---
+++ models/Init.js
@@ -0,0 +1,43 @@
+const Configstore = require('configstore');
+const config = new Configstore('organizze');
+const figlet = require('figlet');
+const inquirer = require('inquirer');
+const show = () => new Promise((resolve, reject) => {
+  figlet('Organizze', (err, data) => {
+    if (err) {
+      return reject(err);
+    }
+
+    return resolve(data);
+  });
+});
+
+module.exports = async () => {
+  console.log(await show());
+
+  const questions = [
+    {
+      type: 'input',
+      name: 'username',
+      message: 'Enter your username',
+    },
+    {
+      type: 'input',
+      name: 'token',
+      message: 'Enter your token',
+      description: 'You can get your token here: https://app.organizze.com.br/configuracoes/api-keys'
+    },
+  ];
+
+  let r = await inquirer.prompt(questions);
+
+  if (r.username.length === 0 || r.token.length === 0) {
+    throw new Error('You need to authenticate first!');
+  }
+
+  config.set('username', r.username);
+  config.set('token', r.token);
+
+  return r;
+};
+
